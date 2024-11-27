86ec84c4d5fe97613f78e966e70f12f037989974
Julio Cesar Leyva <julio101290@users.noreply.github.com>
2024-11-27 12:22:22 -0700

    Update empresas.php

--- src/Views/empresas.php
+++ src/Views/empresas.php
@@ -1,4 +1,4 @@
-<?= $this->include('load/toggle') ?>
+<?= $this->include('julio101290\boilerplate\Views\load/toggle') ?>
 <?= $this->include('julio101290\boilerplate\Views\load\select2') ?>
 <?= $this->include('julio101290\boilerplate\Views\load\datatables') ?>
 <?= $this->include('julio101290\boilerplate\Views\load\nestable') ?>
